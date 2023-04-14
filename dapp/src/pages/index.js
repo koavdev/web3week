6fba89c7b64a9a09b778b65c58c9aba1767be0e3
koavdev <koavdev@gmail.com>
2023-04-14 15:46:41 -0300

    c3 - finish DonateCrypto project

--- dapp/src/pages/index.js
+++ dapp/src/pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Footer from '@/components/Footer';
 import { doLogin } from '@/services/Web3Service';
 import { shortenAddress } from '@/utils/shortenAddress';
+import Link from 'next/link';
 
 
 
@@ -28,29 +29,49 @@ export default function Home() {
       </Head>
       <div className='container px-4 py-5'>
         <div className='row flex-lg-row-reverse align-items-center py-5 g-5'>
-          <div className='col-10 col-sm-8 col-lg-6'>
-            <img className='d-block mx-lg-auto img-fluid' height="500" src='https://images.unsplash.com/photo-1520694478166-daaaaec95b69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'/>
-          </div>
+          {
+            !wallet
+            ? (
+              <div className='col-10 col-sm-8 col-lg-6'>
+                <img className='d-block mx-lg-auto img-fluid' height="500" src='https://images.unsplash.com/photo-1520694478166-daaaaec95b69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'/>
+              </div>
+            )
+            : (
+              <div className='col-10 col-sm-8 col-lg-6'>
+                <p className='mb-3'>What you want to do?</p>
+                <div className='col-12'>
+                  <p><Link href='/donate' className='btn btn-primary col-6 p-3'>Send a Donate</Link></p>
+                  <p><Link href='/create' className='btn btn-secondary col-6 p-3'>Create a New Campaign</Link></p>
+                </div>
+              </div>
+            )
+          }
+          
           <div className='col-lg-6'>
             <h1 className='display-5 fw-bold text-body-emphasis lh-1 mb-3'>Donate Crypto</h1>
             <p className='lead'>Your decentralized donations platform.</p>
             <p className='lead mb-3'>Connect your wallet, create your campaign or donate for already exists.</p>
             <div className='d-grid gap-2 d-md-flex justify-content-md-start'>
-              {!wallet ? (
+              {
+                !wallet
+                ? (
+          
                 <button type='button' className='btn btn-primary btn-lg px-4 me-md-2' onClick={btnLoginClick()}>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/1200px-MetaMask_Fox.svg.png" width="64" className='me-3' />
-                  Conectar com a Metamask
+                  Connect your Metamask
                 </button> 
               ) : (
                 <button type='button' className='btn btn-primary btn-lg px-4 me-md-2'>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/1200px-MetaMask_Fox.svg.png" width="64" className='me-3' />
                   Connected on: {shortenAddress(wallet)}
                 </button>
+                
+                  
               )}
                            
             </div>
 
-            {error}
+            
           </div>
         </div>
         <Footer>
--- dapp/src/services/Web3Service.js
+++ dapp/src/services/Web3Service.js
@@ -1,4 +1,7 @@
 import Web3 from 'web3';
+import ABI from '../services/ABI.json'
+
+const CONTRACT_ADDRESS = '0x11631d3c97B72E637C6DDBAfac0eD080c2ED50ec';
 
 export async function doLogin(){
 
@@ -8,5 +11,33 @@ export async function doLogin(){
     const accounts = await web3.eth.requestAccounts();
     if(!accounts || !accounts.length) throw new Error("Wallet not found/allowed.");
 
+    localStorage.setItem("wallet", accounts[0]);
+
     return accounts[0];
+}
+
+function getContract(){
+    const web3 = new Web3(window.ethereum);
+    const from = localStorage.getItem("wallet");
+    return new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from })
+}
+
+export function addCampaign(campaign){
+    const contract = getContract();
+    return contract.methods.addCampaign(campaign.title, campaign.description, campaign.videoUrl, campaign.imageUrl).send();
+}
+
+export function getLastCampaignId(){
+    const contract = getContract();
+    return contract.methods.nextId().call();
+}
+
+export function getCampaign(id){
+    const contract = getContract();
+    return contract.methods.campaigns(id).call();
+}
+
+export function donate(id, donation){
+    const contract = getContract();
+    return contract.methods.donate(id).send({ value: Web3.utils.toWei(donation, "ether") });
 }
